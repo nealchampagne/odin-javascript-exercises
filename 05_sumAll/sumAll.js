d08ee75688d15f68a90b3ca22dba2fbf93c1a1a5
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-04-30 09:48:32 -0700

    Edit sumAll file to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = (num1, num2) => {
+  let sum = 0;
+  if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+    return 'ERROR';
+  }
+  if (num1 <= num2 && num1 >= 0) {
+    for (i = num1; i < num2 + 1; i++) {
+      sum += i;
+  }} else if (num2 < num1 && num2 >= 0) {
+    for (i = num2; i < num1 + 1; i++) {
+      sum += i;
+    }
+  } else {
+    return 'ERROR';
+  }
+  return sum;
 };
 
 // Do not edit below this line
