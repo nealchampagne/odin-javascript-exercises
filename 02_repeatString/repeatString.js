e0df0c352e696b574266d9a5b7642e9ad1ecc0e1
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-04-30 09:04:25 -0700

    Write code to pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = (str, num) => {
+  let echoedString = ''
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < num; i++) {
+    echoedString += str;
+  }
+  return echoedString;
 };
 
 // Do not edit below this line
