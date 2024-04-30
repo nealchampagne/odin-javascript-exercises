e37fa560f746929bcb25f85b81418a7410a84387
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-04-30 10:04:21 -0700

    Edit tempConversion file to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = (temp) => {
+  temp = (temp - 32) * (5/9);
+  if (temp % 1 === 0) {
+    return Number(temp)
+  } else {
+    return Number(temp.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (temp) => {
+  temp = (temp) * (9/5) + 32;
+  if (temp % 1 === 0) {
+    return Number(temp)
+  } else {
+    return Number(temp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
