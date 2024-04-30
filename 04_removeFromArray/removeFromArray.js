e480cf4709db33d06f55779ccfa0610f851054ad
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-04-30 09:34:30 -0700

    Edit removeFromArray file to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = (...args) => {
+  for (let i = 1; i < args.length; i++) {
+    for (let j = 0; j < args[0].length; j++) {
+      if (args[i] === args[0][j]) {
+        args[0].splice(j, 1);
+        j--;
+      }
+    }
+  }
+  return args[0];
 };
 
 // Do not edit below this line
