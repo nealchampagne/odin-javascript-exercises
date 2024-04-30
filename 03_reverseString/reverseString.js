36ec885f834db435adebc6fb53717a165a65983d
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-04-30 09:10:50 -0700

    Edit reverseString file to pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = (str) => {
+  let backwardString = '';
+  for (let i = 1; i < str.length + 1; i++) {
+    backwardString += str[str.length - i];
+  }
+  return backwardString;
 };
 
 // Do not edit below this line
