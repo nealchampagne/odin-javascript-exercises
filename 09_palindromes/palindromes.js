481ca14c1c7843420f9742170f1b6114c03da6a6
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-05-06 15:21:17 -0700

    Solve exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = string => {
+  let stringArray = [...string.toLowerCase()].filter(c => 
+    ('a' <= c && c <= 'z') || ('0' <= c && c <= '9'));
+  return stringArray.join('') === stringArray.slice().reverse().join('');
 };
 
 // Do not edit below this line
