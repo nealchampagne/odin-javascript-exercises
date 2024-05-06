cd83d0cb4a7eacd336b3696d5ef9466e4698f669
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-05-06 15:43:40 -0700

    Solve exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = num => {
+  console.log(Number(num));
+  if (Number(num) < 0) {
+    return 'OOPS';
+  } else if (Number(num) === 0) {
+    return 0;
+  } else {
+    let num1 = 0;
+    let num2 = 1;
+    let sum = 1;
+    for (let i = 1; i < Number(num); i++) {
+      sum = num1 + num2;
+      num1 = num2;
+      num2 = sum;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
