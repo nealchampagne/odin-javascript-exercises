840a274afc12a7d088891190632bf17c4f634096
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-05-06 14:45:50 -0700

    Solve exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const sum = arr => arr.reduce((acc, cur) => acc + cur, 0);
+
+const multiply = arr => arr.reduce((acc, cur) => acc * cur, 1);
+
+const power = (num1, num2) => {
+  let val = 1;
+  for (let i = 0; i < num2; i++) {
+    val *= num1;
+  }
+  return val;
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = num => {
+  if (num === 0) {
+    return 1;
+  } else {
+    let val = 1;
+    for (i = 0; i < num; i++) {
+      val *= i + 1;
+    }
+    return val;
+  }
 };
 
 // Do not edit below this line
