67b3157d37a00572ac6a6eac1fee720fd90e44fb
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-05-06 15:49:33 -0700

    Solve exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = (obj) =>
+  obj.reduce((arr, value) => {
+    arr.push(value.title);
+    return arr;
+  }, []);
 
 // Do not edit below this line
 module.exports = getTheTitles;
