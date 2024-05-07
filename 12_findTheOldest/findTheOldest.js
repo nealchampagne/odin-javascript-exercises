8b2eca28ae2daa9a9e45a3479301963f59a0843d
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-05-06 19:47:46 -0700

    Solve exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+let currentYear = new Date().getFullYear();
 
+const findTheOldest = (arr) => {
+  arr.sort((a, b) => getAge(a.yearOfDeath, a.yearOfBirth) 
+    > getAge(b.yearOfDeath, b.yearOfBirth) ? -1 : 1);
+  return arr[0];
 };
+
+const getAge = (death, birth) => death ? death - birth 
+  : currentYear - birth;
 
 // Do not edit below this line
 module.exports = findTheOldest;
